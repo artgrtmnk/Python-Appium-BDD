b7dac63392f4fbb3836605489c08bc5f31c0704c
artgrtmnk <78636533+artgrtmnk@users.noreply.github.com>
2022-07-17 19:45:16 +0400

    Changing PageObjects definition, Adding screenshots for fails, Readme typos fix

--- features/environment.py
+++ features/environment.py
@@ -1,20 +1,32 @@
-from appium import webdriver
-from allure_behave.hooks import allure_report
-from datetime import datetime
 import glob
 import os
+from datetime import datetime
+
+import allure
+from allure_commons.types import AttachmentType
+from allure_behave.hooks import allure_report
+from appium import webdriver
+
+from pages.home_page import HomePage
+from pages.add_weight_page import AddWeightPage
+from pages.record_page import RecordPage
 
 
 def before_feature(context, feature):
+    reports = glob.glob('features/artifacts/reports/*')
+    screenshots = glob.glob('features/artifacts/screenshots/*')
+    folders = [reports, screenshots]
+
     try:
-        files = glob.glob('features/artifacts/*')
-        for f in files:
-            os.remove(f)
+        for folder in folders:
+            for file in folder:
+                os.remove(file)
     except:
         pass
 
     context.driver = webdriver.Remote(
-        command_executor='http://127.0.0.1:4723/wd/hub',  # you need to specify your ip and port if it is different (same as in Appium)
+        # you need to specify your ip and port in the next line if it is different (same as in Appium)
+        command_executor='http://127.0.0.1:4723/wd/hub',
         desired_capabilities={
             "platformName": "Android",
             "platformVersion": "",  # you need to specify your Android version
@@ -27,10 +39,21 @@ def before_feature(context, feature):
         }
     )
 
+    # Page objects definition
+    context.home_page = HomePage(context)
+    context.add_weight_page = AddWeightPage(context)
+    context.record_page = RecordPage(context)
 
-def after_scenario(context, feature):
+
+def after_scenario(context, scenario):
     test_datetime = datetime.now().strftime("%d%m%Y%H%M%S")
-    context.driver.save_screenshot("features/artifacts/success %s.png" % test_datetime)
+    screenshot_name = f"{scenario.name}_{scenario.status}_{test_datetime}.png"
+
+    context.driver.save_screenshot(f"features/artifacts/screenshots/{screenshot_name}")
+    if scenario.status == 'failed':
+        allure.attach(context.driver.get_screenshot_as_png(),
+                      name=screenshot_name,
+                      attachment_type=AttachmentType.PNG)
 
 
 def after_feature(context, feature):
